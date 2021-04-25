a17f05f2265ad87e580bb89e2f5c97d00cbcf763
SajalSood <sajal.sood@gmail.com>
2021-04-25 12:17:42 -0400

    infra fixes

--- infra/003-eks-cluster.tf
+++ infra/003-eks-cluster.tf
@@ -20,7 +20,7 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-group-1"
-      instance_type                 = "t2.small"
+      instance_type                 = "t2.medium"
       additional_userdata           = "echo foo bar"
       asg_desired_capacity          = 2
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
